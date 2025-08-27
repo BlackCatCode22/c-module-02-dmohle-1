aab3b34ccf47cdbaf08354fa026924f5e3a3e8bd
dmohle <10038913+dmohle@users.noreply.github.com>
2025-08-27 12:16:02 -0700

    Add files via upload

--- reverseAstring.cpp
+++ reverseAstring.cpp
@@ -4,7 +4,27 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <cctype>
 using namespace std;
+
+
+
+string trim(const string& strIn) {
+    string trimmed_s = strIn;
+
+    // Trim leading whitespace
+    trimmed_s.erase(trimmed_s.begin(), std::find_if(trimmed_s.begin(), trimmed_s.end(), [](unsigned char ch) {
+        return !std::isspace(ch);
+    }));
+
+    // Trim trailing whitespace
+    trimmed_s.erase(std::find_if(trimmed_s.rbegin(), trimmed_s.rend(), [](unsigned char ch) {
+        return !std::isspace(ch);
+    }).base(), trimmed_s.end());
+
+    return trimmed_s;
+}
+
 
 int main() {
     cout << "\n\n Welcome to My Reverse a String program!\n\n";
@@ -16,9 +36,33 @@ int main() {
     reverse(myStrToReverse.begin(), myStrToReverse.end());
     cout << "\n My reversed string is: " << myStrToReverse;
     // Part 2
-    // Use a for loop to swap characters
-    for (int i = 0; i < 10000; i++) {
-        cout << "\n the value of i is " << i << "\n";
-    }
+    // Various string methods
+
+    // Create a string object
+    string myStr01 = "   this is a literal string   ";
+    // Find the length method
+
+    
+
+    int strlength = 0;
+
+    strlength = myStr01.length();
+    // Output the length of the string:
+    cout << "\n The length of my myStr01 is: " << strlength;
+    // Output the string object's value.
+    cout << "\n\n myStr01 is:" << myStr01 << "\n";
+
+    // trim the empty spaces
+    string myTrimmedStr = trim(myStr01);
+
+    // find the string object's length
+    strlength = myTrimmedStr.length();
+    // output the length of the string:
+    cout << "\n The length of my myTrimmedStr is: " << strlength;
+    // Output the trimmed string:
+    cout << "\n myTrimmedStr is:" << myTrimmedStr << "\n\n";
+
+
+
     return 0;
 }
